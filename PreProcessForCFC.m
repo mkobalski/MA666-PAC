83b17330c3d44c767ec540aecb12c220e56e326f
samjlevy <sjl273@cornell.edu>
2016-11-29 12:50:18 -0500

    just the filename and idea

---
+++ PreProcessForCFC.m
@@ -0,0 +1,16 @@
+function [timeStamps, LFsignal, HFsignal]=PreProcessForCFC(t,x,LF,HF)
+%Single unified script to preprocess input and return output signals
+%that can be run on any of our methods
+%INPUT: t - time stamps, expected to start at dt not 0
+%        x - unfiltered EEG/LFP signal
+%        LF - low frequency (maybe band?)
+%        HF - high frequency (maybe band?)
+%        
+%OUTPUT: timeStamps - same as t
+%        LFsignal - filtered low frequency data
+%        HFsignal - filtered high frequency data
+        
+        
+
+
+end
