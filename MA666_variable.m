89fde8c970b63aac09f02aab66ab3d61d5ceef21
cmikkelsen <catmikkelsen@gmail.com>
2016-11-29 13:43:51 -0500

    added comments and events

--- MA666_variable.m
+++ MA666_variable.m
@@ -1,3 +1,6 @@
+%%Creates a simulated signal that will change in coupling strength based on
+%%events given in a
+
 t = [0:.001:60]; % interval for longer simulations
 ts=[0:.001:2]; % sample interval (time)
 f1=5; %lower frequency
@@ -14,15 +17,18 @@ dist=A3*sin(2*pi*42*ts); %disturbance at 42 Hz over ts (2 seconds)
 b=0.2; % amplitude of compound higher frequency
 a=ones(1,2001);
 a(1,200:220)=5;
-% a(1,400:420)=5;
+% a(1,400:410)=5;
 a(1,600:620)=5;
-%a(1,800:820)=5;
+%a(1,800:810)=5;
 a(1,1000:1020)=5;
-%a(1,1200:1220)=5;
+%a(1,1200:1210)=5;
 a(1,1400:1420)=5;
-%a(1,1600:1620)=5;
+%a(1,1600:1610)=5;
 a(1,1800:1820)=5;
 
 sim=low+b*(a.*low.*high+dist);  % simulated data
+sim=sim+randn(size(sim))*0.2;
 figure()
 plot(ts,sim)
+
+events=[200 600 1000 1400 1800];
